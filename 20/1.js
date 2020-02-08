c8635fc36d4539a3e0595c646684148d3e90ef31
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-08 19:08:35 +0900

    리뷰반영

--- 20/1.js
+++ 20/1.js
@@ -2,8 +2,8 @@ function fn(n){
     //문자로 바꾸기
     //쪼개서 역순
     //새배열
-    const arr = n.toString().split('').reverse().map(s => Number(s))
-return arr
+   return n.toString().split('').reverse().map(s => Number(s))
+
 }
 
 console.log(fn(12345))

66eb50d3dccfe92fe8e6271b82d8ecb775bf70a5
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-14 21:40:47 +0900

    리뷰 반영

--- 23/1.js
+++ 23/1.js
@@ -1,4 +1,8 @@
 function fn(n){
-    return Math.sqrt(n) % 2 ? -1 : Math.pow(Math.sqrt(n)+1, 2)
+    // return Math.sqrt(n) % 2 ? -1 : Math.pow(Math.sqrt(n)+1, 2)
+  const num = Math.sqrt(n);
+  return Number.isInteger(num) ? Math.pow(num + 1, 2) : -1;
 }
-console.log(fn(4))
+console.log(fn(11))
+console.log(fn(100))
+console.log(fn(3))

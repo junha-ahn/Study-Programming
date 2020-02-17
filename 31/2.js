aec3f3dc3b9c9b972843c0d8681d3238671cda5d
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-17 22:25:13 +0900

    2번 오답 수정

--- 31/2.js
+++ 31/2.js
@@ -2,14 +2,11 @@ function star() {
     const n = 5;
     let str = ''
     for (let i = 1; i <= n; i++) {
-      for (let j = 1; j < 6 - i; j++) {
+      for (let j = 1; j <=n-i; j++) {
         str +=  ' '
       }
-      for (let k = 1; k <= 2 * i - 1; k++) {
+      for (let k = 1; k <= i; k++) {
         str += '*'
-      }
-      for(let l = 1; l <6 -i; l++){
-          str+= ' '
       }
       str += '\n'
     }

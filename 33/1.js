7d6c22132d61f99df097a6f1fe075a8dd1292668
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-21 22:43:39 +0900

    리뷰 반영

--- 33/1.js
+++ 33/1.js
@@ -1,6 +1,14 @@
+//reduce
 const sum = (arr) => {
     return arr.reduce((p,c) => p+c)
 }
+
 console.log(sum([1,2,3,4,5]))
 console.log(sum([1]))
 console.log(sum([1,2]))
+
+//재귀함수
+function fn(arr) {
+    return arr.length === 0 ? 0 : arr[0] + fn(arr.slice(1))
+  }
+console.log(fn([1, 2, 3]));

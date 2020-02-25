3151aee0de6907ac43d9594eb7a5118f57c88d41
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-25 22:24:22 +0900

    ...수정?

--- 47/1.js
+++ 47/1.js
@@ -1,3 +1,24 @@
-const arr = [1, 2, 3, 4, 5, 6]
-console.log(arr.map(e => e * 2))
-console.log(arr.filter(e => e % 2 === 0))
+
+function _map(arr) {
+    const arr2 = []
+    for(let i = 0; i < arr.length; i++){
+        arr2.push(arr[i]*2)
+    }
+    return arr2
+
+}
+
+function _filter(arr) {
+    const arr2 = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0 ) arr2.push(arr[i])
+    }
+    return arr2
+
+}
+
+// console.log(map([1, 2, 3, 4, 5 , 6], e => e * 2))
+// console.log(filter([1, 2, 3, 4, 5 , 6], e => e % 2 === 0))
+
+console.log(_map([1,2,3,4,5]))
+console.log(_filter([1,2,3,4,5]))

d19518ab62ffe78a8f92ef0ef75e352b6927fadf
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-26 22:22:15 +0900

    리뷰 또 반영....?

--- 47/1.js
+++ 47/1.js
@@ -1,24 +1,23 @@
-
-function _map(arr) {
-    const arr2 = []
-    for(let i = 0; i < arr.length; i++){
-        arr2.push(arr[i]*2)
+const _map = arr => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(arr[i] * 2);
     }
-    return arr2
-
-}
-
-function _filter(arr) {
-    const arr2 = []
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] % 2 === 0 ) arr2.push(arr[i])
+    return result;
+  };
+  
+  const _filter = arr => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) result.push(arr[i]);
     }
-    return arr2
-
-}
-
-// console.log(map([1, 2, 3, 4, 5 , 6], e => e * 2))
-// console.log(filter([1, 2, 3, 4, 5 , 6], e => e % 2 === 0))
-
-console.log(_map([1,2,3,4,5]))
-console.log(_filter([1,2,3,4,5]))
+    return result;
+  };
+  
+  const someFunction = (arr, fn) => {
+    return fn(arr);
+  };
+  
+  console.log(someFunction([1, 2, 3, 4, 5], _map));
+  console.log(someFunction([1, 2, 3, 4, 5], _filter));
+  

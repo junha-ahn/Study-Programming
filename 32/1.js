d63db7baab66290dee62d91352544fa9a8910af5
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-27 18:37:01 +0900

    별찍기

--- 32/1.js
+++ 32/1.js
@@ -1,10 +1,10 @@
 
 const star = (n) => {
 for(let i = 0; i < n; i ++){
-    console.log(" ".repeat(n - (i+1)) + "*".repeat(i*2 + 1))
+    console.log(' '.repeat(n - (i+1)) + '*'.repeat(i*2 + 1))
 }
-for(let j = n-1; j >= 1; j--){
-    console.log(" ".repeat(n-j)+"*".repeat(j*2-1))
+for(let j = n-1; j >= 1; j --){
+    console.log(' '.repeat(n - j) + '*'.repeat(j*2 - 1))
 }
 }
 

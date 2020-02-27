2667ec59f00ab927a6b94c2d5eee8f698fefd08e
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-27 20:10:38 +0900

    코드 수정

--- 35/2.js
+++ 35/2.js
@@ -5,8 +5,15 @@ function fnFor(arr) {
     }
     return sum / arr.length;
   }
-  function fn(arr) {
-    return arr.reduce((p, c) => p + c) / arr.length;
+  // function fn(arr) {
+  //   return arr.reduce((p, c) => p + c) / arr.length;
+  // }
+
+  function fn(arr){
+    const sum = eval(arr.join('+'))
+    const avg = sum / arr.length
+    return avg  
   }
+
   console.log(fnFor([1, 2, 3, 4, 5]));
   console.log(fn([1, 2, 3, 4, 5]));

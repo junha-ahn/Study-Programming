e674e514d0b3aee14aa5bfe44b0edbf052cd3666
Lilian-Kim <lilian.k3104@gmail.com>
2020-03-12 23:24:43 +0900

    0311 답

---
+++ 70/1.js
@@ -0,0 +1,16 @@
+// let node = { type: 'Identifier' };
+
+// // // 1번. destructuring 을 이용해서 node의 type을 새로운 newType 변수에 초기화해보세요
+
+// const {type: newType} = node
+// console.log(newType)
+
+let node = {
+  type: 'Identifier',
+  name: 'foo'
+  }, 
+  type = 'Literal',
+  name = 5;
+  ({type: type} = node)
+ console.log(type)
+// 2번. destructuring 을 이용해서 node의 type을 type 변수에 담아보세요
